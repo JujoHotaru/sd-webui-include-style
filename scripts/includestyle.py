7e67eb02c6a69630a5434cbadeb88c65a7445410
Hotaru Jujo <satsukijujo@gmail.com>
2024-01-14 16:41:41 +0900

    Update

--- scripts/includestyle.py
+++ scripts/includestyle.py
@@ -8,6 +8,7 @@ class IncludeStyleScript(scripts.Script):
 
     original_styles_ : object = None
 
+    # 現在のバージョンではUIは無い
     def title(self):
         return "IncludeStyle"
     def show(self, is_img2img):
@@ -17,16 +18,18 @@ class IncludeStyleScript(scripts.Script):
 
     def _replace_common(self, prompt, negative):
 
+        # include指定をすべて探す
         founds = (re.findall(RE_PATTERN, prompt))
         for found in founds:
-            key = str.strip(found[1])
+            key = str.strip(found[1]) # keyにスタイル名が入る
             if key in shared.prompt_styles.styles:
-                if(negative):
+                if(negative): # Negativeプロンプトの場合
                     content = str.strip(shared.prompt_styles.styles[key].negative_prompt)
-                else:
+                else: # Positiveプロンプトの場合
                     content = str.strip(shared.prompt_styles.styles[key].prompt)
                 #print(f"[IncludeStyle] Includes style \"{key}\" : \"{content}\"")
             else:
+                # includeで指定したスタイルが見つからない
                 print(f"[IncludeStyle] Warning: Style \"{key}\" not found!")
                 content = ""
             prompt = prompt.replace(found[0], content)
@@ -40,6 +43,7 @@ class IncludeStyleScript(scripts.Script):
         #pprint.pprint(vars(p))
         #pprint.pprint(vars(p.scripts))
 
+        # ADetailer用プロンプトの置換はこのタイミングで行う
         if p.extra_generation_params and "ADetailer prompt" in p.extra_generation_params:
             #print("_replace_common() for p.extra_generation_params[ADetailer prompt]")
             p.extra_generation_params["ADetailer prompt"] = self._replace_common(p.extra_generation_params["ADetailer prompt"], False)
@@ -54,10 +58,12 @@ class IncludeStyleScript(scripts.Script):
 
     def before_process(self, p, *args):
 
-        import pprint
+        #import pprint
         #pprint.pprint(vars(p))
 
-        # 処理中、メモリ上のスタイルデータを変更し、処理が終わったら戻すため、オリジナルデータをコピーしておく
+        # 他拡張機能より優先してinclude処理を行うため、processではなくbefore_processで置換処理を行う。
+        # スタイルはこの時点ではプロンプトに反映されていないため、スタイルマネージャー側に記録されているデータを置換してしまう。
+        # 処理が終わったら戻すため、オリジナルデータをコピーしておく
         self.original_styles_ = shared.prompt_styles.styles.copy()
 
         # メモリ上のスタイルデータのincludeをすべて解決（再帰対応）
